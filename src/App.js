df3f204a23d976a43d44debcc9775f0dc988aedc
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-04-26 11:14:11 +0100

    User/Login/Register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { UsernameForm } from "./components/usernameForm";
+import Navbar from "./components/Navbar";
+import { tokenCheck } from "./utils";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.key("myToken")) {
+      tokenCheck(setUser);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full">
+      <nav classname="nav">
+        <Navbar setShow={setShow} />
+      </nav>
+
+      <UsernameForm
+        setUser={setUser}
+        user={user}
+        onClose={() => setShow(false)}
+        show={show}
+      />
+      <div className="mainContent">
+        <div className="App"></div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
