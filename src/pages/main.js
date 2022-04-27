73a3275505943d43a4320ebf794647dc3af013f0
mor94n <ollertonmorgan@outlook.com>
2022-04-27 12:12:13 +0100

    refactored todo

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,21 @@
 import React from "react";
+import ToDoTemplate from "../components/utilsComponents/todo";
+import { useState } from "react";
 
 
 const John = () => {
+    const [toDo, setToDo] = useState(false);
     return ( 
-
+        
 
         <div>
             <h1>hello</h1>
+            {toDo && <ToDoTemplate/>}
+            <button onClick = {()=>setToDo(!toDo)}> ToDo </button>
         </div>
      );
 }
  
 export default John;
+
+
