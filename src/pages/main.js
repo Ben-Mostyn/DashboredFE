1b469398b23a782f684d8ccc32011aed2c65fb5c
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-04-27 14:27:25 +0100

    implemented removeHandler on textbox - errors still need looking into

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,11 +2,12 @@ import React from "react";
 import ToDoTemplate from "../components/utilsComponents/todo";
 import { useState } from "react";
 import Textbox from "../components/utilsComponents/textbox";
+import "./main.css";
 
 const John = () => {
   const [toDo, setToDo] = useState(false);
   return (
-    <div>
+    <div className="playArea">
       <h1>hello</h1>
       {toDo && <ToDoTemplate />}
       <button onClick={() => setToDo(!toDo)}> ToDo </button>
