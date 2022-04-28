4ad664199d357d57e119116f4f1c0ea65af329b4
Greydayrhys <rhysanthonyward@hotmail.co.uk>
2022-04-28 10:19:10 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 const Navbar = () => {
   return (
     <div className="nav">
-     
+     <h1>hellooo</h1>
     </div>
   );
 };
--- src/pages/main.js
+++ src/pages/main.js
@@ -4,16 +4,21 @@ import { useState } from "react";
 import Textbox from "../components/utilsComponents/textbox";
 import "./main.css";
 import ImageUpload from "../components/utilsComponents/image_upload";
+import Navbar from "../components/Navbar";
 
 const ScrapBook = () => {
   const [toDo, setToDo] = useState(false);
   return (
+
+    <div className= "container">
+<Navbar></Navbar>
     <div className="playArea">
       <h1>hello</h1>
       {toDo && <ToDoTemplate />}
       <button onClick={() => setToDo(!toDo)}> ToDo </button>
       <Textbox />
       <ImageUpload />
+    </div>
     </div>
   );
 };
