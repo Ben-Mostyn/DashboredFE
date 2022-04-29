3a2ba13a6b36146b5463506794d73fdb3bf60baa
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-04-29 15:57:01 +0100

    images saving to cloudinary

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,21 +3,19 @@ import ToDoTemplate from "../components/utilsComponents/todo";
 import { useState } from "react";
 import Textbox from "../components/utilsComponents/textbox";
 import "./main.css";
- apiTest
+// import App from "../utils/index";
+
 import MemeGenerator from "../components/utilsComponents/memeApi";
 
+import ImageHandle from "../components/utilsComponents/image-loader/image_upload";
+import NavBar from "../components/Navbar";
 
-import Upload from "../components/utilsComponents/image-loader/image_upload";
-import  NavBar from "../components/Navbar";
-
-
-import {BiText} from "react-icons/bi"
+import { BiText } from "react-icons/bi";
 import { color } from "@cloudinary/url-gen/qualifiers/background";
 
 const ScrapBook = ({ user, setUser }) => {
-dev
-
   const [toDo, setToDo] = useState(false);
+  const [image, setImage] = useState();
 
   //Testing
   const [showBtn, setShowBtn] = useState(false);
@@ -25,31 +23,34 @@ dev
   const [textInput, setTextInput] = useState();
 
   return (
-<div>
-  <div className="main">
-<NavBar setShowBtn={setShowBtn} setTextArea={setTextArea} setTextInput={setTextInput} textInput={textInput} textArea={textArea}/>
-    <div className="playArea">
-      <button
-        onClick={() => {
-          setUser();
-          localStorage.clear();
-        }}
-      >
-       
-        Logout
-      </button>
-      <h1>hello!</h1>
-      {toDo && <ToDoTemplate />}
-      <button onClick={() => setToDo(!toDo)}> ToDo </button>
-      <Textbox />
- apiTest
-      <MemeGenerator/>
+    <div>
+      <div className="main">
+        <NavBar
+          setShowBtn={setShowBtn}
+          setTextArea={setTextArea}
+          setTextInput={setTextInput}
+          textInput={textInput}
+          textArea={textArea}
+        />
+        <div className="playArea">
+          <button
+            onClick={() => {
+              setUser();
+              localStorage.clear();
+            }}
+          >
+            Logout
+          </button>
+          <h1>hello!</h1>
+          {toDo && <ToDoTemplate />}
+          <button onClick={() => setToDo(!toDo)}> ToDo </button>
+          <Textbox />
+          <MemeGenerator />
 
-      <Upload />
- dev
+          <ImageHandle />
+        </div>
+      </div>
     </div>
-     </div>
-     </div>
   );
 };
 
